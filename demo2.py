80e13f1299a2aca7e5cfac707af8b9b791390402
adamwillerton <131967086+adamwillerton@users.noreply.github.com>
2026-02-06 12:01:53 +0000

    demo2.py

---
+++ demo2.py
@@ -0,0 +1,8 @@
+# interaction.py
+name = input("Enter your name: ")
+print(f"Welcome, {name}!")
+
+with open("greeting.txt", "w") as f:
+    f.write(f"User {name} accessed this environment.\n")
+
+print("Greeting saved to file.")
