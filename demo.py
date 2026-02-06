b2142b9bda3ff6718742b6cd308d4265e09b1c91
adamwillerton <131967086+adamwillerton@users.noreply.github.com>
2026-02-06 11:52:01 +0000

    added new py file

---
+++ demo.py
@@ -0,0 +1,7 @@
+# hello.py
+import os
+print("Program starting...")
+print("Running on:", os.uname())
+print("Process ID:", os.getpid())
+print("Working directory:", os.getcwd())
+print("Program complete.")
